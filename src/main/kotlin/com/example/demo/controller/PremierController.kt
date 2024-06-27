fc990edd70a754e6ba0eb7d0aadd33d19f6034aa
a <a>
2024-06-27 11:48:09 -0300

    n

--- src/main/kotlin/com/example/demo/controller/PremierController.kt
+++ src/main/kotlin/com/example/demo/controller/PremierController.kt
@@ -1,13 +1,14 @@
 package com.example.demo.controller
 
+import com.example.demo.`interface`.PremierService
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class PremierController {
+class PremierController (private val premierService: PremierService) {
 
     @GetMapping("/premier")
     fun premier():String{
-        return "premier"
+        return premierService.getPremierService()
     }
 }
